9c69e4a2f548206c0452598713f280edbc122e5f
Adrián López <adrianlomu2004@gmail.com>
2023-03-09 11:36:13 +0100

    (build): implementar maven y información kata

---
+++ romans_go_home/src/test/java/edu/craptocraft/romans_go_home/RomansGoHomeTest.java
@@ -0,0 +1,20 @@
+package edu.craptocraft.romans_go_home;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RomansGoHomeTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
